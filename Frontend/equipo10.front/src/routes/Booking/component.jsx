fc5f409ee9b84ab653013c106e15d036038d24f2
mcamila1098 <maria-alvarezp@unilibre.edu.co>
2023-07-24 18:59:53 -0500

    Uploading final project

---
+++ Frontend/equipo10.front/src/routes/Booking/component.jsx
@@ -0,0 +1,10 @@
+import ConfirmBookingForm from "../../components/Booking/confirmBooking/component";
+
+const Booking = () =>{
+
+  return(
+    <ConfirmBookingForm />
+  )
+}
+
+export default Booking;
